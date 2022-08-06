d783cb98d509401ffd3ccaa55ef13fd93ab6550d
Admin <Admin@DESKTOP-1ERB0H2>
2022-08-06 23:32:30 +0530

    Second Commit

---
+++ Assignment-1/src/com/tns/looping/PatternProgram.java
@@ -0,0 +1,18 @@
+package com.tns.looping;
+
+public class PatternProgram {
+
+	public static void main(String[] args) {
+	int i,j,r=5;
+	for(i=0;i<r;i++) 
+	{
+		for(j=0;j<=i;j++)
+		{
+			System.out.print("@");
+		}
+		System.out.println();
+		
+	}
+	}
+
+}
